c08c8efd80edea7303154f0768918be3cd6a6d4d
joshu2761 <joshua.y0761@gmail.com>
2023-10-17 09:13:42 -0400

    password thing

--- Program1.java
+++ Program1.java
@@ -1,12 +1,32 @@
 class Program1 extends ConsoleProgram {
 
   /**
-  * Description
-  * @author:
+  * While loops
+  * @JoshuaYin:
   */
   
   public void run() {
-    
-    
+
+    String password = "password123";
+    int tries = 5;
+
+    while(tries > 0){
+
+      String passwordGuess = readLine("Enter the password: ");
+
+      if(passwordGuess.equals(password)){
+        System.out.println("Correct!");
+        break;
+      }
+      else {
+        tries--; 
+        if(tries != 0){
+          System.out.println("Incorrect. You have " + tries + " tries left");
+        }
+        else{
+          System.out.println("Incorrect, you have used up too many tries");
+        }
+      }
+    }
   }
 }
